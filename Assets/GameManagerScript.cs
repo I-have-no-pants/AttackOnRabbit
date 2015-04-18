fe46674dbbf21fb46c738b32074a59719ad6808e
Jesper Tingvall <jesper@tingvall.pw>
2015-04-18 20:03:23 +0200

    Add blood explosion

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class GameManagerScript : MonoBehaviour {
 
+	public int Score;
+	public int Combo;
+
+	public float ComboTime;
+	private float ComboTimer;
+
+	public void BunnyKill(int i) {
+		Score += i * Combo;
+		Combo++;
+		ComboTimer = ComboTime;
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +22,10 @@ public class GameManagerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (ComboTimer > 0)
+			ComboTimer -= Time.deltaTime;
+		else
+			Combo = 0;
+
 	}
 }
