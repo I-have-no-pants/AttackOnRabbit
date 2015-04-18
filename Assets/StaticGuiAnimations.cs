7ab9da060de62a1acc293c9ffa171ec438e19d02
Jesper Tingvall <jesper@tingvall.pw>
2015-04-18 21:48:18 +0200

    Static GUI animatons

---
+++ Assets/StaticGuiAnimations.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StaticGuiAnimations : MonoBehaviour {
+
+	public Text Score;
+	public Text Killstreak;
+	public Text Message;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Score.text = "Score\n" + 0;
+		Killstreak.text = "Killstreak\n" + 0;
+	}
+
+	public void EndGame() {
+
+	}
+
+	public void ShowMessage(string m) {
+		Message.text = m;
+		GetComponent<Animator> ().SetTrigger ("Message");
+	}
+}
