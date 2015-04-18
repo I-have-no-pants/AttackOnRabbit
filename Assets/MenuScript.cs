5db788ffacc734e89e2dff4f1ed4582645a36b36
Jesper Tingvall <jesper@tingvall.pw>
2015-04-18 23:14:49 +0200

    Main menu fade away

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1"))
+		    GetComponent<Animator>().SetTrigger("Start");
+	}
+
+	public void NextLevel() {
+		Application.LoadLevel (1);
+	}
+
+
+}
