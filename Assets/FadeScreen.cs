763afc410dae531b0e5322f47ac9840cf11a9e50
Adam <adagr407@student.liu.se>
2015-04-18 20:44:07 +0200

    Menu Screen

---
+++ Assets/FadeScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeScreen : MonoBehaviour {
+
+	private float alpha = 0;
+	private bool startGame = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1"))
+			startGame = true;
+		if (startGame) {
+			if (alpha > 1)
+				Application.LoadLevel("plane");
+			alpha += Time.deltaTime/2;
+			Color color = gameObject.GetComponent<MeshRenderer>().material.color;
+			color.a = alpha;
+			gameObject.GetComponent<MeshRenderer>().material.color = color;
+		}
+	}
+}
---
+++ Assets/rotatePlane.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatePlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.RotateAround(gameObject.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
