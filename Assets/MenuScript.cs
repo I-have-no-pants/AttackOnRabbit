db4ff8bf0b396a10037069cdf6583fdb40edc543
Jesper Tingvall <jesper@tingvall.pw>
2015-04-19 11:32:40 +0200

    Add score to menu

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
+
+
+	public Text score;
+	public Text highscore;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +17,9 @@ public class MenuScript : MonoBehaviour {
 	void Update () {
 		if (Input.GetButtonDown("Fire1"))
 		    GetComponent<Animator>().SetTrigger("Start");
+
+		highscore.text = "Best\n" + ScoreScript.highScore;
+		score.text = "Score\n" + ScoreScript.previousHighScore;
 	}
 
 	public void NextLevel() {
